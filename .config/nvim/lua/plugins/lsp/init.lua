7a811d24558ca22ac83607e421e714a1d3070d46
smjonas <jonas.strittmatter@gmx.de>
2023-01-25 22:19:56 +0100

    Try using lsp-zero

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,6 +1,7 @@
 local M = {
-  "neovim/nvim-lspconfig",
+  "VonHeikemen/lsp-zero.nvim",
   dependencies = {
+    "neovim/nvim-lspconfig",
     "folke/neodev.nvim",
     "ray-x/lsp_signature.nvim",
     {
@@ -9,13 +10,6 @@ local M = {
         require("mason").setup()
       end,
       dependencies = { "williamboman/mason-lspconfig.nvim" },
-    },
-    {
-      "jose-elias-alvarez/null-ls.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function()
-        require("plugins.lsp.null_ls")
-      end,
     },
   },
 }
@@ -28,53 +22,34 @@ M.config = function()
   -- Note: not all LSP servers support range formatting
   -- map("v", "<C-f>", vim.lsp.buf.range_formatting, { silent = true })
 
-  -- Modified from hrsh7th/cmp-nvim-lsp/
-  -- This avoids a dependency of this module on cmp_nvim_lsp.
-  local update_capabilities = function(capabilities)
-    local completionItem = capabilities.textDocument.completion.completionItem
-    completionItem.snippetSupport = true
-    completionItem.preselectSupport = true
-    completionItem.insertReplaceSupport = true
-    completionItem.labelDetailsSupport = true
-    completionItem.deprecatedSupport = true
-    completionItem.commitCharactersSupport = true
-    completionItem.tagSupport = { valueSet = { 1 } }
-    completionItem.resolveSupport = {
-      properties = { "documentation", "detail", "additionalTextEdits" },
-    }
-    return capabilities
-  end
-
-  -- Contain keymappings to set when server attached
-  local on_attach = require("plugins.lsp.on_attach")
-  local capabilities = update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
+  local lsp = require("lsp-zero")
   local setup_lsp_servers = function()
-    local ok, mason = pcall(require, "mason-lspconfig")
+    local ok, _ = pcall(require, "mason-lspconfig")
     if not ok then
       vim.notify("Mason was not found: no LSP servers set up", vim.log.levels.ERROR)
       return
     end
-    mason.setup {
-      ensure_installed = { "pylsp", "gopls" },
-    }
-    local lsp_config = require("lspconfig")
+
+    lsp.preset("recommended")
+    lsp.ensure_installed { "pylsp", "gopls" }
 
     local opts = {
-      on_attach = on_attach,
-      capabilities = capabilities,
+      -- Contains keymappings to set when server is attached
+      on_attach = require("plugins.lsp.on_attach"),
       single_file_support = true,
     }
-    for _, server in ipairs(mason.get_installed_servers()) do
-      local customized_servers = { "sumneko_lua", "pylsp" }
-      if vim.tbl_contains(customized_servers, server) then
-        opts = vim.tbl_deep_extend("force", {}, require("plugins.lsp." .. server))
-      end
-      lsp_config[server].setup(opts)
-    end
-  end
+    -- Set a default configuration
+    lsp.set_server_config(opts)
 
+    local customized_servers = { "sumneko_lua", "pylsp" }
+    for _, server in ipairs(customized_servers) do
+      local custom_opts = vim.tbl_deep_extend("force", {}, require("plugins.lsp." .. server))
+      lsp.configure(server, custom_opts)
+    end
+    lsp.setup { manage_nvim_cmp = false }
+  end
   setup_lsp_servers()
+  lsp.set_sign_icons { info = "", hint = "", warn = "", error = "" }
 
   -- Better hover window colors
   local normal_float_bg = vim.fn.synIDattr(vim.fn.hlID("NormalFloat"), "bg")
@@ -84,24 +59,6 @@ M.config = function()
   vim.diagnostic.config {
     virtual_text = false,
   }
-
-  local diagnostic_symbols = {
-    Error = "",
-    Information = { "", "DiagnosticSignInfo" },
-    Hint = "",
-    Info = "",
-    Warn = "",
-  }
-
-  for prefix, icon in pairs(diagnostic_symbols) do
-    local is_table = type(icon) == "table"
-    local hl = is_table and icon[2] or "DiagnosticSign" .. prefix
-    vim.fn.sign_define("DiagnosticSign" .. prefix, {
-      text = is_table and icon[1] or icon,
-      texthl = hl,
-      numhl = "Comment",
-    })
-  end
 
   -- Borders around lsp windows
   -- local popup_opts = { border = "single", focusable = false, max_width = 60 }
