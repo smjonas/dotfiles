9bba45b6bbb2f8bdc7c088d6714558593609ee1c
smjonas <jonas.strittmatter@gmx.de>
2023-01-25 22:19:03 +0100

    refactor: move null-ls

---
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,25 @@
+local M = {
+  "jose-elias-alvarez/null-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+}
+
+M.config = function()
+  local null_ls = require("null-ls")
+  local formatting = null_ls.builtins.formatting
+
+  require("null-ls").setup {
+    sources = {
+      formatting.stylua,
+      formatting.prettier.with {
+        extra_args = { "--bracket-same-line", "true", "--print-width", "110" },
+        filetypes = { "css", "html", "javascript", "json", "xhtml" },
+      },
+      formatting.black.with { extra_args = { "--line-length", "130" } },
+      -- formatting.isort.with({ extra_args = { "--profile black" } }),
+      formatting.isort,
+      formatting.gofmt,
+    },
+  }
+end
+
+return M
