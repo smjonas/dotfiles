448cfbdf44083d789885a8a59d7f263107a5b6b6
smjonas <jonas.strittmatter@gmx.de>
2023-03-14 16:21:24 +0100

    fix(vimtex): VimtexCompile mapping

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -3,6 +3,11 @@ return {
   ft = "tex",
   config = function()
     -- vim.cmd("autocmd User VimtexEventInitPost VimtexCompile")
-    vim.keymap.set("n", "<leader>c", vim.cmd.VimtexCompile, { buffer = true })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VimtexEventInitPost",
+      callback = function()
+        vim.keymap.set("n", "<leader>c", vim.cmd.VimtexCompile, { buffer = true })
+      end,
+    })
   end,
 }
