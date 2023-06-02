3ad23998cc6a1b26f6c444e1cf6e42e140bf4c21
smjonas <jonas.strittmatter@gmx.de>
2023-06-02 12:09:23 +0200

    fix(markdown-preview.nvim): also enable for vimwiki files

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,10 +1,10 @@
 return {
   "iamcco/markdown-preview.nvim",
   build = "cd app && npm install",
-  ft = { "markdown", "text" },
+  ft = { "markdown", "vimwiki", "text" },
   config = function()
-    vim.g["mkdp_auto_close"] = 0
-    vim.g["mkdp_filetypes"] = { "markdown", "text" }
-    map("n", "<leader>m", "<Plug>MarkdownPreviewToggle", { noremap = false })
+    vim.g.mkdp_auto_close = 0
+    vim.g.mkdp_filetypes = { "markdown", "vimwiki", "text" }
+    vim.keymap.set("n", "<leader>m", "<Plug>MarkdownPreviewToggle")
   end,
 }
