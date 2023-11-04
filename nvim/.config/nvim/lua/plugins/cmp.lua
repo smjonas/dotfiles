45b31bbf821672e40b1cb0e27cebcc81f9fc577b
smjonas <jonas.strittmatter@gmx.de>
2023-11-04 10:46:12 +0100

    Only accept cmp suggestion with <C-s> for now

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -112,13 +112,13 @@ M.config = function()
     mapping = {
       -- mostly keep defaults except use <C-f> instead <C-y>
       -- and overload tab keys for snippet plugins
-      ["<C-f>"] = cmp.mapping(
-        cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        },
-        { "i", "s" }
-      ),
+      -- ["<C-f>"] = cmp.mapping(
+      --   cmp.mapping.confirm {
+      --     behavior = cmp.ConfirmBehavior.Replace,
+      --     select = true,
+      --   },
+      --   { "i", "s" }
+      -- ),
       ["<C-s>"] = require("piantor").cmp_confirm(cmp),
       ["<Tab>"] = cmp.mapping(function(fallback)
         if luasnip.expand_or_locally_jumpable() then
