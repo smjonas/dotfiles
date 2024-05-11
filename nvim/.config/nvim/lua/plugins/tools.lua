3ad9dfa80fdce1d2870f757a280230c89df9e6c2
smjonas <jonas.strittmatter@gmx.de>
2024-05-11 13:42:55 +0200

    feat: add grug-far.nvim for search and replace

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -1,4 +1,10 @@
 return {
   { "sindrets/diffview.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
   { "zbirenbaum/copilot.lua", cmd = "Copilot", event = "InsertEnter", config = true },
+  {
+    "MagicDuck/grug-far.nvim",
+    config = function()
+      require("grug-far").setup()
+    end,
+  },
 }
