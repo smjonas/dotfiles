4969c2fa31d9475709fbc0f1b95758137588d08e
smjonas <jonas.strittmatter@gmx.de>
2024-09-07 23:05:24 +0200

    feat(cmp-jira): also show authored commits not on todo

--- nvim/.config/nvim/lua/plugins/cmp-jira.lua
+++ nvim/.config/nvim/lua/plugins/cmp-jira.lua
@@ -2,6 +2,11 @@ return {
   "smjonas/cmp-jira",
   ft = "gitcommit",
   config = function()
-    require("cmp_jira").setup { file_types = { "gitcommit" } }
+    require("cmp_jira").setup {
+      file_types = { "gitcommit" },
+      jira = {
+        jql = '(assignee=%s+OR+reporter=%s)+AND+resolution=unresolved+AND+status+!=+"TO+DO"',
+      },
+    }
   end,
 }
