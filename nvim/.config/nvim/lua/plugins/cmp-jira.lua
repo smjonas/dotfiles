ae85e6365e152ad971187a375b1c71d7f00d7dcf
smjonas <jonas.strittmatter@gmx.de>
2024-09-08 00:06:21 +0200

    feat(cmp-jira): only enable for certain commit edit messages

--- nvim/.config/nvim/lua/plugins/cmp-jira.lua
+++ nvim/.config/nvim/lua/plugins/cmp-jira.lua
@@ -4,8 +4,13 @@ return {
   config = function()
     require("cmp_jira").setup {
       file_types = { "gitcommit" },
+      enabled = function()
+        local file_name = vim.fn.expand("%:p")
+        local enable_pattern = ".*/NeoCargo/.*/COMMIT_EDITMSG"
+        return file_name:find(enable_pattern) ~= nil
+      end,
       jira = {
-        jql = '(assignee=%s+OR+reporter=%s)+AND+resolution=unresolved+AND+status+!=+"TO+DO"',
+        jql = '(assignee=%s+OR+reporter=%s)+AND+resolution=unresolved+AND+status+!=+"TO+DO"+ORDER+BY+updated+DESC',
       },
     }
   end,
