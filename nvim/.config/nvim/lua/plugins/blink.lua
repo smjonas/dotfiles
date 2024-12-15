4996d316ce838347785bbed9fe487a4e57117b57
smjonas <jonas.strittmatter@gmx.de>
2024-12-15 23:13:16 +0100

    feat: use blink.cmp to reenable cmp_jira source

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,27 +1,50 @@
 return {
-  "saghen/blink.cmp",
-  lazy = false,
-  version = "v0.*",
-  dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
-  config = {
-    keymap = {
-      preset = "default",
-      ["<C-s>"] = { "select_and_accept" },
+  {
+    "saghen/blink.compat",
+    lazy = true,
+    opts = {},
+  },
+  {
+    "saghen/blink.cmp",
+    lazy = false,
+    version = "v0.*",
+    dependencies = {
+      { "L3MON4D3/LuaSnip", version = "v2.*" },
+      { "smjonas/cmp-jira" },
     },
-    completion = { accept = { auto_brackets = { enabled = true } } },
-    snippets = {
-      expand = function(snippet)
-        require("luasnip").lsp_expand(snippet)
-      end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require("luasnip").jumpable(filter.direction)
-        end
-        return require("luasnip").in_snippet()
-      end,
-      jump = function(direction)
-        require("luasnip").jump(direction)
-      end,
+    config = {
+      keymap = {
+        preset = "default",
+        ["<C-s>"] = { "select_and_accept" },
+      },
+      completion = {
+        accept = { auto_brackets = { enabled = true } },
+      },
+      sources = {
+        completion = {
+          enabled_providers = { "lsp", "path", "luasnip", "buffer", "cmp_jira" },
+        },
+        providers = {
+          cmp_jira = {
+            name = "cmp_jira",
+            module = "blink.compat.source",
+          },
+        },
+      },
+      snippets = {
+        expand = function(snippet)
+          require("luasnip").lsp_expand(snippet)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require("luasnip").jumpable(filter.direction)
+          end
+          return require("luasnip").in_snippet()
+        end,
+        jump = function(direction)
+          require("luasnip").jump(direction)
+        end,
+      },
     },
   },
 }
