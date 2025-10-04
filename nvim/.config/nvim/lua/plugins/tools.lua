4fefb9d69380588b7faa13ea0738c025410cf5f9
smjonas <jonas.strittmatter@gmx.de>
2025-10-04 14:47:16 +0200

    chore: disable copilot

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -1,6 +1,6 @@
 return {
   { "sindrets/diffview.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
-  { "zbirenbaum/copilot.lua", cmd = "Copilot", event = "InsertEnter", config = true },
+  { "zbirenbaum/copilot.lua", cmd = "Copilot", event = "InsertEnter", config = true, enabled = false },
   {
     "MagicDuck/grug-far.nvim",
     config = function()
